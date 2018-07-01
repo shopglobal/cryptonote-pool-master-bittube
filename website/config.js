8cf8e06a38357dc8c863c0a1165462d83f145521
HeroMiners <admin@herominers.com>
2018-07-01 23:12:44 +0300

    Initial commit

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "https://pool.sumokoin.ch:8888";
+
+var poolHost = "pool.sumokoin.ch";
+
+var irc = "irc.freenode.net/#sumokoin";
+
+var email = "support@sumokoin.ch";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.sumokoin.com/block/{id}";
+
+var transactionExplorer = "http://explorer.sumokoin.com/tx/{id}";
+
+var themeCss = "themes/default-theme.css";
